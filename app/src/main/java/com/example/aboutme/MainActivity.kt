cc0d3c04556a74a0f338fde56daf45e440eba92e
chiqvu1 <chiquynhvu268@gmail.com>
2019-09-26 14:03:17 -0700

    Created Kotlin code to display nickname when user hit Done. After button Done is clicked, button and edit text visibility will be gone. However, the view of user input will be visible.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var nickNameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+        editText = findViewById(R.id.nicknam_edit)
+        nickNameTextView = findViewById(R.id.nickname_text)
+    }
+
+    private fun addNickname(view: View) {
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility= View.GONE
+        nickNameTextView.visibility = View.VISIBLE
+
     }
 }
